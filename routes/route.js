2a384989bfe9f6e47988b429f6e7610943ba0de9
Zida01 <olaty21bisi@gmail.com>
2023-09-07 17:36:19 +0100

    HNGx_stage_01

--- routes/route.js
+++ routes/route.js
@@ -6,20 +6,20 @@ const router = require('express').Router();
 
 router.get('/api', (req, res) => {
     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const day = new Date()
+    const day = new Date();
 
     const data = {
         slack_name: req.query.slack_name,
         current_day: weekday[day.getDay()],
-        utc_time: day,
+        utc_time: new Date().toISOString().slice(0, 19) + 'Z',
         track: req.query.track,
         github_file_url: "https://github.com/Zida01/HNGx_stage_01/blob/main/routes/route.js",
         github_repo_url: "https://github.com/Zida01/HNGx_stage_01",
         status_code: 200,
     }
-    return res.json({
+    return res.json(
         data
-    })
+    )
 })
 
 
